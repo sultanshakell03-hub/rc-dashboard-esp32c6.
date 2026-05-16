a8cc839aa5bfefaebb9b4417b6fc26ff6af6ed9e
sultanshakell03-hub <sultan.shakell03@gmail.com>
2026-05-16 11:09:16 +0700

    Update dashboard_rc.ino

--- dashboard_rc.ino
+++ dashboard_rc.ino
@@ -1,29 +1,9 @@
-name: Build ESP32-C6 Firmware
+#include <Arduino.h>
 
-on:
-  workflow_dispatch:
+void setup() {
+  Serial.begin(115200);
+}
 
-jobs:
-  build:
-    runs-on: ubuntu-latest
-
-    steps:
-      - uses: actions/checkout@v4
-
-      - uses: arduino/setup-arduino-cli@v2
-
-      - run: |
-          arduino-cli core update-index
-          arduino-cli core install esp32:esp32
-
-      - run: |
-          mkdir -p build
-          arduino-cli compile \
-            --fqbn esp32:esp32:esp32c6 \
-            --output-dir build \
-            dashboard_rc.ino
-
-      - uses: actions/upload-artifact@v4
-        with:
-          name: firmware
-          path: build/*.bin
+void loop() {
+  delay(1000);
+}
